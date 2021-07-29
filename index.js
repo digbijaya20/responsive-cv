23ceb6f4d00f137355f56337035d84941e4fa3fb
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2021-07-29 23:09:00 +0530

    login and signup ui

---
+++ index.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+function topRegistrationBtn(){
+    x.style.left ="-400px";
+    y.style.left ="50px";
+    z.style.left ="100px";
+}
+
+function topLoginBtn(){
+    x.style.left ="50px";
+    y.style.left ="450px";
+    z.style.left ="0px";
+}
